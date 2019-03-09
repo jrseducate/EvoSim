ec6cabbd9f2c751973bc3386165678d0ff972606
Jeremy <jrseducate@gmail.com>
2019-03-09 17:27:03 -0500

    EvoSim 1.0

---
+++ js.js
@@ -0,0 +1,345 @@
+declareClass('Expression', function(expMap)
+{
+    this.expMap = expMap;
+}, {
+    getExpMap: function()
+    {
+        return this.expMap;
+    },
+    getExpKeys: function()
+    {
+        return Object.keys(this.expMap);
+    },
+    getExpVals: function()
+    {
+        return Object.values(this.expMap);
+    },
+    getRandomExpKey: function()
+    {
+        var expKeys = this.getExpKeys(),
+            i       = Math.floor(Math.random() * expKeys.length);
+
+        return expKeys[i];
+    },
+    getRandomExp: function()
+    {
+        var expKey = this.getRandomExpKey();
+
+        return {
+            key: expKey,
+            val: this.expMap[expKey],
+        };
+    },
+});
+
+var startedAt = Math.floor(Date.now() / 1000);
+
+declareClass('DNA', function(expression, options) {
+    this.options    = options;
+    this.exp        = expression;
+    this.dnaInc     = 0;
+    this.avgFitness = 0;
+    this.cntFitness = 0;
+    this.len        = try_get(options, 'len', try_get(options, 'dna.length', null));
+    this.dna        = try_get(options, 'dna', this.randomDna(), is_array);
+    this.activate   = try_get(options, 'activate', function(){throw 'this.activate is not defined!';}, is_function);
+    this.update     = try_get(options, 'update', function(){throw 'this.update is not defined!';}, is_function);
+    this.render     = try_get(options, 'render', function(){throw 'this.render is not defined!';}, is_function);
+
+    (this.init      = try_get(options, 'init', function(){}, is_function)).apply(this);
+}, {
+    randomDna: function()
+    {
+        var res = [];
+
+        if(!isset(this.exp))
+        {
+            throw 'DNA.exp must be set.';
+        }
+        if(!isset(this.len))
+        {
+            throw 'Either DNA.len or DNA.dna must be set.';
+        }
+
+        each(range(0, this.len), function(i)
+        {
+            res.push(this.exp.getRandomExp());
+        });
+
+        return res;
+    },
+    toString: function()
+    {
+        return JSON.stringify(this.getDna(), null, 2);
+    },
+    getDna: function()
+    {
+        return this.dna;
+    },
+    calcFitness : function()
+    {
+        return ((Math.min(600, this.y) / 600) + (Math.min(800, this.x) / 800)) / 2;
+    },
+    mutate    : function()
+    {
+        var selectedIndex = Math.floor(Math.random() * this.dna.length);
+
+        this.dna[selectedIndex] = this.exp.getRandomExp();
+    },
+    crossover : function(other)
+    {
+        if(other instanceof DNA)
+        {
+            var newDna = [],
+                split  = Math.floor((Math.random() * (Math.min(this.dna.length, other.dna.length) / 2)) + (Math.min(this.dna.length, other.dna.length) / 4));
+
+            for(var i = 0; i < Math.min(this.dna.length, other.dna.length); i++)
+            {
+                if(i < split)
+                {
+                    newDna.push(this.dna[i]);
+                }
+                else
+                {
+                    newDna.push(other.dna[i]);
+                }
+            }
+
+            return New(DNA, this.exp, Object.assign(this.options, {dna: newDna}));
+        }
+
+        return this;
+    }
+});
+
+declareClass('Population', function(options)
+{
+    this.size   = try_get(options, 'size', 100);
+    this.dnaDef = try_get(options, 'dnaDef', {}, is_object);
+    this.dnaExp = try_get(options, 'dnaExp', {}, is_object);
+    this.dnaList = this.generateDnaList();
+}, {
+    init : function()
+    {
+        each(this.dnaList, function(dna)
+        {
+            dna.init();
+        });
+    },
+    generateDnaList : function()
+    {
+        var dnaList = [];
+
+        for(var i = 0; i < this.size; i++)
+        {
+            dnaList.push(New(DNA, this.dnaExp, this.dnaDef));
+        }
+
+        return dnaList;
+    },
+    reproduce : function()
+    {
+        var fitList = [];
+
+        each(this.dnaList, function(dna)
+        {
+            var fitness = dna.avgFitness,
+                count   = Math.min(fitness * 100) + 1;
+
+            for(var i = 0; i < count; i++)
+            {
+                fitList.push(dna);
+            }
+        });
+
+        this.dnaList = [];
+
+        for(var i = 0; i < this.size; i++)
+        {
+            var selectedIndex  = Math.floor(Math.random() * fitList.length),
+                selectedDna    = fitList[selectedIndex],
+                selectedIndexB = Math.floor(Math.random() * fitList.length),
+                selectedDnaB   = fitList[selectedIndexB];
+
+            this.dnaList.push(selectedDna.crossover(selectedDnaB));
+        }
+
+        this.init();
+    },
+    render : function()
+    {
+        return each(this.dnaList, function(dna)
+        {
+            dna.render();
+        });
+    },
+    update : function()
+    {
+        return each(this.dnaList, function(dna)
+        {
+            dna.update();
+        });
+    },
+});
+
+var fitnessSpan = document.getElementById('app-fitness'),
+    canvas      = document.getElementById('app-canvas'),
+    graphics    = canvas.getContext('2d'),
+    backMax     = 25,
+    backMaxDist = 50,
+    forwMax     = 25,
+    forwMaxDist = 50,
+    exp         = New(Expression, {
+        up    : [0, 1],
+        down  : [0, -1],
+        left  : [-1, 0],
+        right : [1, 0],
+        back  : function()
+        {
+            this.inc    = try_get(this, 'inc', Math.floor(Math.random() * backMax) + 2) - 1;
+            this.dist   = try_get(this, 'dist', Math.floor(Math.random() * backMaxDist) + 2);
+
+            return this.inc > 0 ? this.dist : 0;
+        },
+        // forw  : function()
+        // {
+        //     this.inc    = try_get(this, 'inc', Math.floor(Math.random() * forwMax) + 1) - 1;
+        //     this.dist   = try_get(this, 'dist', Math.floor(Math.random() * forwMaxDist) + 1);
+        //
+        //     return this.inc > 0 ? this.dist : 0;
+        // }
+    }),
+    pop = New(Population, {
+        size   : 150,
+        dnaExp : exp,
+        dnaDef : {
+            len      : 50,
+            init     : function()
+            {
+                this.dnaInc = 0;
+                this.x      = 400;
+                this.y      = 300;
+                this.color  = try_get(this, 'color', getRandomColor() + '44', is_string);
+            },
+            activate : function()
+            {
+                var expression = this.dna[this.dnaInc];
+
+                if(isset(expression))
+                {
+                    var res = true,
+                        inc = this.dnaInc;
+
+                    switch(typeof expression.val)
+                    {
+                        case 'function':
+                            switch(expression.key)
+                            {
+                                case 'forw':
+                                    this.dnaInc = Math.min(this.dna.length - 1, this.dnaInc + expression.val());
+                                    break;
+                                case 'back':
+                                    this.dnaInc = Math.max(0, this.dnaInc - expression.val());
+                                    break;
+                            }
+                            break;
+                        default:
+                            res = expression;
+                            break;
+                    }
+
+                    if(inc === this.dnaInc)
+                    {
+                        this.dnaInc++;
+                    }
+
+                    if(res === true)
+                    {
+                        return this.activate();
+                    }
+
+                    return res;
+                }
+
+                return false;
+            },
+            update : function()
+            {
+                if(this.x <= 0 || this.x >= 800 || this.y <= 0 || this.y >= 600)
+                {
+                    return true;
+                }
+
+                var expression = this.activate();
+
+                if(is_array(expression.val) && expression.val.length === 2)
+                {
+                    this.x += expression.val[0] * 15;
+                    this.y += expression.val[1] * 15;
+                }
+
+                this.avgFitness = ((this.avgFitness * this.cntFitness) + this.calcFitness()) / ++this.cntFitness;
+
+                if(Math.random() < 0.01)
+                {
+                    this.mutate();
+                }
+
+                return expression !== false;
+            },
+            render : function()
+            {
+                graphics.fillStyle = this.color;
+                graphics.fillRect(this.x - 15, this.y - 15, 30, 30);
+            },
+        }
+    });
+
+var i        = 0,
+    interval = setInterval(function()
+{
+    graphics.clearRect(0, 0, 800, 600);
+
+    graphics.fillStyle = '#FFFFFF';
+    graphics.fillRect(0, 0, 800, 600);
+
+    graphics.fillStyle = '#FF0000';
+    graphics.fillRect(750, 550, 50, 50);
+
+    graphics.fillStyle = '#000000';
+    graphics.font      = "15px Arial";
+    graphics.textAlign = "center";
+    graphics.fillText("Dest", 775, 580);
+
+    pop.render();
+    pop.update();
+
+    if(i++ >= 120)
+    {
+        var avgFitness = 0,
+            breakCount = fitnessSpan.querySelectorAll('br');
+
+        each(pop.dnaList, function(dna)
+        {
+            avgFitness += dna.avgFitness;
+        });
+
+        avgFitness = avgFitness / pop.dnaList.length;
+
+        if(breakCount.length >= 10)
+        {
+            var secondBreak       = fitnessSpan.innerHTML.lastIndexOf('<br>');
+            fitnessSpan.innerHTML = fitnessSpan.innerHTML.slice(0, secondBreak);
+        }
+
+        fitnessSpan.innerHTML = "<br>Average Fitness: " + (avgFitness * 100).toFixed(2) + fitnessSpan.innerHTML;
+
+        i = 0;
+        pop.reproduce();
+    }
+}, 25);
+
+function stop()
+{
+    clearInterval(interval);
+}
---
+++ util.js
@@ -0,0 +1,682 @@
+//----------------------------
+//  Utilities
+//----------------------------
+
+/**
+* Is Boolean
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_boolean(v)
+{
+  return typeof v === 'boolean';
+}
+
+/**
+* Is Integer
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_integer(v)
+{
+  return (v ^ 0) === v;
+}
+
+/**
+* Is Numeric
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_numeric(v)
+{
+  return !isNaN(v);
+}
+
+/**
+* Is String
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_string(v)
+{
+  return typeof v === 'string';
+}
+
+/**
+* Is Function
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_function(v)
+{
+  return typeof v === 'function';
+}
+
+/**
+* Is Function
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_object(v)
+{
+  return typeof v === 'object' && !is_array(v);
+}
+
+/**
+* Is Function
+*
+* @param {*} v
+* @return {boolean}
+*/
+function is_array(v)
+{
+  return v instanceof Array;
+}
+
+function is_element(v)
+{
+  try {
+    //Using W3 DOM2 (works for FF, Opera and Chrome)
+    return v instanceof HTMLElement;
+  }
+  catch(e){
+    //Browsers not supporting W3 DOM2 don't have HTMLElement and
+    //an exception is thrown and we end up here. Testing some
+    //properties that all elements have (works on IE7)
+    return (typeof v==="object") &&
+      (v.nodeType===1) && (typeof v.style === "object") &&
+      (typeof v.ownerDocument ==="object");
+  }
+}
+
+/**
+* Is Set
+*
+* @param {*} v
+* @return {boolean}
+*/
+function isset(v, k)
+{
+    if(k !== undefined && k !== null)
+    {
+        v = try_get(v, k);
+    }
+    return v !== undefined && v !== null;
+}
+
+/**
+* Defaulted
+*
+* @param {*} val
+* @param {*} def
+* @return {*}
+*/
+function defaulted(val, def, cond)
+{
+  cond = is_function(cond) ? cond : isset;
+  return cond(val) ? val : def;
+}
+
+/**
+* Try Get
+*
+* @param {Object} context
+* @param {string} path
+* @param {*|null} def
+* @return {*}
+*/
+function try_get(context, path, def, cond)
+{
+    path = typeof path === 'string' ? path : null;
+    def  = defaulted(def, null);
+    cond = defaulted(cond, isset, is_function);
+
+    var pathSet = isset(path),
+        hasDot  = pathSet ? path.indexOf('.') !== -1 : false,
+        a       = context,
+        b       = path;
+
+  if(pathSet && isset(a))
+  {
+    if(hasDot)
+    {
+      var dotIndex     = 0,
+          lastDotIndex = dotIndex,
+          lastIndex    = path.lastIndexOf('.'),
+          subContext   = context;
+
+      while((dotIndex = path.indexOf('.', lastDotIndex)) !== -1)
+      {
+        var key = path.slice(lastDotIndex, dotIndex !== -1 ? dotIndex : path.length);
+
+        if(!isset(subContext[key]))
+        {
+          return def;
+        }
+
+        a = subContext = subContext[key];
+        lastDotIndex   = dotIndex;
+
+        if(dotIndex > 0)
+        {
+          lastDotIndex++;
+        }
+      }
+
+      b = path.slice(lastDotIndex, path.length);
+    }
+
+    return defaulted(a[b], def, cond);
+  }
+
+  return def;
+}
+
+/**
+* Try Set
+*
+* @param {Object} context
+* @param {string} path
+* @param {*} val
+* @return {boolean}
+*/
+function try_set(context, path, val)
+{
+  path = typeof path === 'string' ? path : null;
+
+  var pathSet = isset(path),
+      hasDot  = pathSet ? path.indexOf('.') !== -1 : false,
+      a       = context,
+      b       = path;
+
+  if(pathSet)
+  {
+    if(hasDot)
+    {
+      var dotIndex     = 0,
+          lastDotIndex = dotIndex,
+          lastIndex    = path.lastIndexOf('.'),
+          subContext   = context;
+
+      while((dotIndex = path.indexOf('.', lastDotIndex)) !== -1)
+      {
+        var key = path.slice(lastDotIndex, dotIndex !== -1 ? dotIndex : path.length);
+
+        if(!isset(subContext[key]))
+        {
+          return false;
+        }
+
+        a = subContext = subContext[key];
+        lastDotIndex   = dotIndex;
+
+        if(dotIndex > 0)
+        {
+          lastDotIndex++;
+        }
+      }
+
+      b = path.slice(lastDotIndex, path.length);
+    }
+
+    if(isset(a))
+    {
+        a[b] = val;
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+* Define Property
+*
+* @param {Object} context
+* @param {string} propertyPath
+* @param {*} value
+* @return {boolean}
+*/
+function defineProperty(context, propertyPath, value)
+{
+  if(!isset(try_get(context, propertyPath)))
+  {
+    return try_set(context, propertyPath, value);
+  }
+
+  return false;
+}
+
+/**
+* Range
+*
+* @param {int} start
+* @param {int} end
+* @return {Array}
+*/
+function range(start, end)
+{
+    var res = [];
+
+    if(start > end)
+    {
+        return res;
+    }
+
+    for(var i = start; i <= end; i++)
+    {
+        res.push(i);
+    }
+
+    return res;
+}
+
+/**
+* Each
+*
+* @param {Array|object} list
+* @param {function} func
+*/
+function each(list, func)
+{
+    var res = true;
+
+    if((is_array(list) || is_object(list)) && is_function(func))
+    {
+        var keys = is_object(list) ? Object.keys(list) : range(0, list.length - 1);
+        for(var i = 0; i < keys.length && res; i++)
+        {
+            var key = keys[i],
+                val = undefined;
+            try
+            {
+                val = list[key];
+            }
+            catch(e){}
+
+            res &= func(val, key) !== false;
+        }
+    }
+
+    return res;
+}
+
+/**
+* Each Flat
+*
+* @param {object} list
+* @param {function} func
+*/
+function eachFlat(list, func, pathKey)
+{
+    var res = true;
+
+    if(is_object(list) && is_function(func))
+    {
+        res = each(list, function(val, key)
+        {
+            var newPathKey = (isset(pathKey) ? (pathKey + '.') : '') + key;
+
+            if(is_object(val))
+            {
+                return eachFlat(val, func, newPathKey);
+            }
+            else
+            {
+                return func(val, newPathKey) !== false;
+            }
+        });
+    }
+
+    return res;
+}
+
+/**
+* Every Function
+*
+* @return {boolean}
+*/
+function everyFunc()
+{
+    var args = arguments;
+
+    return function()
+    {
+        var res      = true,
+            funcArgs = arguments;
+
+        each(args, function(func)
+        {
+            if(is_function(func))
+            {
+                res &= func.apply(null, funcArgs);
+            }
+        });
+
+        return res;
+    };
+}
+
+/**
+* Any Function
+*
+* @return {boolean}
+*/
+function anyFunc()
+{
+    var args = arguments;
+
+    return function()
+    {
+        var res      = false;
+            funcArgs = arguments;
+
+        each(args, function(func)
+        {
+            if(is_function(func))
+            {
+                res |= func.apply(null, funcArgs);
+            }
+
+            return res === false;
+        });
+
+        return res;
+    };
+}
+
+//----------------------------
+//  Classes
+//----------------------------
+
+/**
+* New Class
+*
+* @param {function} constructor
+* @param {Object} definition
+* @param {Object|null} proto
+* @param {*} def
+* @return {Object}
+*/
+function newClass(constructor, definition, proto)
+{
+  var resClass   = constructor,
+      definition = defaulted(definition, {}, is_object),
+      defKeys    = Object.keys(definition);
+
+  proto = typeof proto === 'function' ? proto : null;
+
+  if(proto !== null)
+  {
+    resClass.prototype        = Object.create(proto.prototype);
+    resClass.prototype.super = function()
+    {
+      proto.apply(this, arguments);
+    };
+  }
+
+  resClass.prototype.constructor = constructor;
+
+  for(var i = 0; i < defKeys.length; i++)
+  {
+    var key = defKeys[i],
+        val = definition[key];
+
+    resClass.prototype[key] = val;
+  }
+
+  return resClass;
+}
+
+/**
+* Declare Class
+*
+* @param {string} className
+* @param {function} constructor
+* @param {Object} definition
+* @param {Object|null} proto
+*/
+function declareClass(className, constructor, definition, proto)
+{
+  defineProperty(window, className, newClass(constructor, definition, proto));
+}
+
+/**
+* New Object
+*
+* @param func
+* @return {Object|function}
+*/
+function New(func)
+{
+    var args = Array.prototype.slice.call(arguments, 1),
+        res  = {};
+
+    if (func.prototype !== null)
+    {
+        res.__proto__ = func.prototype;
+    }
+
+    var ret = func.apply(res, args);
+
+    if (is_object(ret) || is_function(ret))
+    {
+        return ret;
+    }
+
+    return res;
+}
+
+function getRandomColor()
+{
+    var letters = '0123456789ABCDEF',
+        color   = '#';
+        
+    for (var i = 0; i < 6; i++)
+    {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+//----------------------------
+//  Debug
+//----------------------------
+
+function mergeObjects(a, b, p)
+{
+    p = defaulted(p, true, is_boolean);
+
+    var newObj = {};
+
+    each(a, function(v, k)
+    {
+        newObj[k] = v;
+    });
+    each(b, function(v, k)
+    {
+        newObj[k] = v;
+    });
+
+    if(p)
+    {
+        newObj.__proto__ = mergeObjects(try_get(a, '__proto__', {}, is_object), try_get(b, '__proto__', {}, is_object), false);
+    }
+
+    return newObj;
+}
+
+function flattenProtos(obj, limit, __depth)
+{
+    limit   = defaulted(limit, 50, is_integer);
+    __depth = defaulted(__depth, 0, is_integer);
+
+    if(isset(obj, '__proto__') && obj.__proto__ !== obj && __depth < limit)
+    {
+        return mergeObjects(obj, flattenProtos(obj.__proto__, limit, __depth + 1), false);
+    }
+
+    return obj;
+}
+
+function isDebugObject(obj)
+{
+    return try_get(obj, 'isDebugObject', false, is_boolean);
+}
+
+function debugObject(obj, options)
+{
+    var dObj = {};
+
+    dObj.__proto__ = {
+        __proto__: defaulted(dObj.__proto__, {}, is_object)
+    };
+
+    each(obj, function(val, key)
+    {
+        Object.defineProperty(dObj, key, {
+            get: function() {
+                return this.__get__(key);
+            },
+            set: function(value) {
+                this.__set__(key, value);
+            },
+        });
+    });
+
+    var protos = flattenProtos(obj.__proto__);
+
+    each(protos, function(val, key)
+    {
+        if(val === undefined)
+        {
+            var propertyOpts = Object.getOwnPropertyDescriptor(protos, key);
+            Object.defineProperty(dObj.__proto__, key, {
+                get: function() {
+                    return this.__get__(key, try_get(propertyOpts, 'get'));
+                },
+                set: function(value) {
+                    this.__set__(key, value, try_get(propertyOpts, 'set'));
+                },
+            });
+        }
+        else
+        {
+            dObj.__proto__[key] = function()
+            {
+                return this.__func__(key, arguments, val);
+            }
+        }
+    });
+
+    dObj.__proto__.__get__ = function(key, override)
+    {
+        var onGet  = try_get(this, 'options.get', function(){}, is_function),
+            getRes = onGet(this, key, override);
+
+        if(getRes === undefined)
+        {
+            if(is_function(override))
+            {
+                getRes = override.call(this.obj);
+            }
+            else
+            {
+                getRes = this.obj[key];
+            }
+        }
+
+        if(is_object(getRes) && !isDebugObject(getRes) && try_get(options, 'chainDebugObject', true, is_boolean))
+        {
+            getRes = debugObject(getRes, this.options);
+        }
+
+        return getRes;
+    }
+
+    dObj.__proto__.__set__ = function(key, value, override)
+    {
+        var onSet  = try_get(this, 'options.set', function(){}, is_function),
+            setRes = onSet(this, key, value, override);
+
+        if(!setRes)
+        {
+            if(is_function(override))
+            {
+                override.call(this.obj, value);
+            }
+            else
+            {
+                this.obj[key] = value;
+            }
+        }
+    }
+
+    dObj.__proto__.__func__ = function(key, args)
+    {
+        var voidOp  = '-' + Math.random() + '-',
+            onFunc  = try_get(this, 'options.func', function(dObj, key, args)
+            {
+                return dObj.obj[key].apply(dObj.obj, args);
+            }, is_function),
+            funcRes = onFunc(this, key, args, voidOp);
+
+        if(funcRes === voidOp)
+        {
+            funcRes = this.obj[key].apply(dObj.obj, args);
+        }
+
+        if(is_object(funcRes) && !isDebugObject(funcRes) && try_get(options, 'chainDebugObject', true, is_boolean))
+        {
+            funcRes = debugObject(funcRes, this.options);
+        }
+
+        return funcRes;
+    }
+
+    dObj.isDebugObject = true;
+    dObj.obj = obj;
+    dObj.options = options;
+
+    return dObj;
+}
+
+// function debugObjectInPlace(obj, options)
+// {
+//     var newObj   = mergeObjects({}, obj),
+//         debugObj = debugObject(obj, options);
+//
+//     console.log(newObj);
+//
+//     each(obj, function(val, key)
+//     {
+//         if(isset(val))
+//         {
+//             newObj[key] = val;
+//             delete obj[key];
+//             Object.defineProperty(obj, key, {
+//                 get: function() {
+//                     return this.__get__(key);
+//                 },
+//                 set: function(value) {
+//                     this.__set__(key, value);
+//                 },
+//             });
+//         }
+//     });
+//
+//     newObj.__proto__ = obj.__proto__;
+//
+//     obj.isDebugObject = true;
+//     obj.obj       = newObj;
+//     obj.options   = debugObj.options;
+//     obj.__proto__ = debugObj.__proto__;
+// }
