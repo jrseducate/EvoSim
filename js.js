100cafb0e1fd681b5f21ec227d7da4ffb4df3625
Jeremy <jrseducate@gmail.com>
2019-03-11 00:33:09 -0400

    EvoSim 1.1.4.8 - Increased count associated with fitness now that fitness is ranked by increasing fitness

--- js.js
+++ js.js
@@ -207,7 +207,7 @@ declareClass('Population', function(options)
         each(this.dnaList, function(dna)
         {
             var fitness = dna.hitBoundary() ? 0 : dna.fitness / pop.tick,
-                count   = Math.floor(fitness * 100);
+                count   = Math.floor(fitness * 500);
 
             for(var i = 0; i <= count; i++)
             {
